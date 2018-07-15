ffc6ac51ad006b0bc1506826f31604e77340cf26
Markus von Rüden <mvr@opennms.com>
2018-07-15 21:48:37 +0200

    Ensure database is recreated before running tests

--- bamboo-specs/scripts/test.sh
+++ bamboo-specs/scripts/test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Prepare test environment
+echo "Preparing Test environment"
+psql -U postgres -h localhost -c 'drop database if exists opennms' || exit
+psql -U postgres -h localhost -c 'create database opennms' || exit
+
 # Tests
 echo "Testing"
 mvn verify -DupdatePolicy=never -DskipTests=false -DskipITs=false --batch-mode || exit
